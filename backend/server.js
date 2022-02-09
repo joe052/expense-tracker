f5cb022024b8fa09a43d3ec2af4d2606bbefa4fe
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-02-09 11:11:37 +0000

    routed some data to backend.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const bodyParser = require('body-parser');
 
 app.use(express.static('backend'));
 app.listen(port,()=>{
   console.log(`App running at ${port} welcome to the dark side!!`);
+});
+
+app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
+
+app.post('/login',(req,res)=>{
+  console.log(req.body);
 });
